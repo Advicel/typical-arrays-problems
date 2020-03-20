a3aa97edcbcc81fe7a2d1d98fa65fc0f39726aa8
unknown <sowetnikov2010@yandex.ru>
2020-03-20 10:08:30 +0300

    init: typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
+const check=(array)=>{
+  if(array==undefined || array.length==0) return false;
+  return true;
+}
+
 exports.min = function min (array) {
+  if(check(array)){
+    let minItem = array[0];
+    for(let item of array){
+      if(item<minItem) minItem=item;
+    }
+    return minItem;
+  } 
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(check(array)){
+    let maxItem = array[0];
+    for(let item of array){
+      if(item>maxItem) maxItem=item;
+    }
+    return maxItem;
+  }
+  return 0
 }
 
 exports.avg = function avg (array) {
+  if(check(array)){
+    let sum =0;
+    for(let item of array){
+      sum+=item;
+    }
+    return sum/array.length;
+  }
   return 0;
 }
